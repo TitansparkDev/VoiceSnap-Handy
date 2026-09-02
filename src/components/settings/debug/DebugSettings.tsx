6721dd0e8755477888b35d4b8a07214cfb97dd77
Titanspark21 <127077745+Titanspark21@users.noreply.github.com>
2026-09-02 17:01:18 +0000

    feat: add cleanup latency diagnostics

--- src/components/settings/debug/DebugSettings.tsx
+++ src/components/settings/debug/DebugSettings.tsx
@@ -14,6 +14,7 @@ import { ClamshellMicrophoneSelector } from "../ClamshellMicrophoneSelector";
 import { UpdateChecksToggle } from "../UpdateChecksToggle";
 import { WhatsNewPreview } from "./WhatsNewPreview";
 import { KeyboardDiagnostic } from "./KeyboardDiagnostic";
+import { CleanupLatencyDiagnostics } from "./CleanupLatencyDiagnostics";
 
 export const DebugSettings: React.FC = () => {
   const { t } = useTranslation();
@@ -43,6 +44,7 @@ export const DebugSettings: React.FC = () => {
         <AlwaysOnMicrophone descriptionMode="tooltip" grouped={true} />
         <ClamshellMicrophoneSelector descriptionMode="tooltip" grouped={true} />
         <KeyboardDiagnostic />
+        <CleanupLatencyDiagnostics />
         <LiveLogViewer descriptionMode="tooltip" grouped={true} />
       </SettingsGroup>
     </div>
